b860dbb0488cc1a18609017090d0c85f63d39589
anjanpal2001 <anjanpal3369@gmail.com>
2026-04-04 00:46:09 +0530

    Add some new feature

--- ATM_Student_Simulation.java
+++ ATM_Student_Simulation.java
@@ -1,33 +1,54 @@
 import java . util.*;
-class Atm{
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class Atm {
     int balance;
-    Atm(int balance){
-        this .balance=balance;
+    LocalDateTime now;
+    String name;
+
+    Atm(int balance, String name) {
+        this.balance = balance;
+        this.name = name;
+        this.now = LocalDateTime.now();
+
+        // Creating a formatter to make the date readable
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
+
+        // Providing the arguments for the format specifiers
+        System.out.printf("Account of %s is created at %s%n", this.name, formattedDate);
     }
-    public void GetBalence(){
-        System.out.println(this.balance);
+
+    public void GetBalance(){
+        now = LocalDateTime.now();
+        System.out.println(this.name+" has current Balance"+this.balance+"At"+now);
     }
     public void deposit(int amount){
         this.balance+=amount;
-        System.out.println("You have successfully Add : "+amount);
+        now = LocalDateTime.now();
+        System.out.println(this.name+" has successfully Add : "+amount+" at "+now);
 
     }
     public void withdraw(int amount){
-        if(this.balance<amount) throw new Error("The insufficient Balance");
+        now = LocalDateTime.now();
+        if(this.balance<amount) throw new Error(this.name+"has  insufficient Balance");
         else {
             this.balance-=amount;
-            System.out.println("you successfully withdraw Rs : "+amount);
+            System.out.println(this.name+" successfully withdraw Rs : "+amount+" at "+now);
         }
     }
 }
 public class ATM_Student_Simulation {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        Atm st1=new Atm(0);
+        Atm st1=new Atm(0,"st1");
 
         while(true){
             System.out.println("Enter a valid operation");
-            System.out.println("1.Deposit \n 2.GetBalence \n 3.withdraw \n 4. Exit ");
+            System.out.println("1.Deposit \n2.GetBalance \n3.withdraw \n4. Exit ");
             int expression=sc.nextInt();
             switch (expression) {
                 case 1:
@@ -38,7 +59,7 @@ public class ATM_Student_Simulation {
                     break;
 
                 case 2:
-                    st1.GetBalence();
+                    st1.GetBalance();
                     break;
                 case 3:
                     System.out.println("Enter amount to withdraw");
